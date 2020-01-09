9ecd5f78ae44349231a7fdf12afc25606a45c972
Schaffer <schaffer@cpqd.com.br>
2020-01-09 08:59:15 -0300

    Estrutura de pastas

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({titulo}) => {
+    return (
+        <button>titulo</button>
+    )
+}
+
+export default Button
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../../components/Button'
+import './style.scss';
+
+function Home() {
+  return (
+    <div className="home-body">
+      Home
+      <Button titulo="Botão"/>
+    </div>
+  );
+}
+
+export default Home;
